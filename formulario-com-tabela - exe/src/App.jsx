0f877caa15a34dff00f3f8bd6abf5b090399e149
Julian Pedro De Alexandre <julian.zep@gmail.com>
2026-01-09 20:37:51 -0300

    feat: implementa estados e props nos componentes, integrado ao App.jsx

--- formulario-com-tabela - exe/src/App.jsx
+++ formulario-com-tabela - exe/src/App.jsx
@@ -1,13 +1,37 @@
 import Form from "./components/form e buttons/Form"
 import Table from "./components/table/Table"
 import "./globals/style.css"
+import { useState } from "react"
 
 function App() {
 
+  const[nome, setNome] = useState("");
+  const[senha, setSenha] = useState("");
+  const[nomeTabela, setNomeTable] = useState("");
+  const[senhaTabela, setSenhaTable] = useState("");
+
+  function cadastrar(){
+     alert("foi")
+  }
+
+  function entrar (){
+    alert("foi")
+  }
+
   return (
     <>
-      <Form/>
-      <Table/>
+      <Form
+      nome={nome}
+      senha={senha}
+      // handleNome={handleNome}
+      // handleSenha={handleSenha}
+      entrar={entrar}
+      cadastrar={cadastrar}
+      />
+      <Table
+      nomeTabela={nomeTabela}
+      senhaTabela={senhaTabela}
+      />
     </>
   )
 }
