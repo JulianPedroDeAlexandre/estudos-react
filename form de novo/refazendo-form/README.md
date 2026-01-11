67a154633cd94d425f81b6c185324ccddc3fa3c1
Julian Pedro De Alexandre <julian.zep@gmail.com>
2026-01-11 14:03:12 -0300

    feat: adiciona useState para os onChange dos inputs

--- form de novo/refazendo-form/README.md
+++ form de novo/refazendo-form/README.md
@@ -4,3 +4,5 @@ criar as pastas para os components
 criar os components e seus css module
 criar estilos globais
 programar o componete Form
+programar componente Table
+programar App
--- form de novo/refazendo-form/src/App.jsx
+++ form de novo/refazendo-form/src/App.jsx
@@ -1,12 +1,32 @@
 import FormButtons from "./components/form-buttons/FormButtons"
 import Table from "./components/table/Table"
 import "./globals/style.css"
+import { useState } from "react"
 
 function App() {
 
+const [nome, setNome] = useState("");
+const [senha, setSenha] = useState("");
+const [nomeTabela, setNomeTabela] = useState("");
+const [senhaTabela, setSenhaTabela] = useState("");
+
+const handleNome = (e) =>{
+  setNome(e.target.value);
+}
+
+const handleSenha = (e) =>{
+  setSenha(e.target.value)
+}
+
   return (
     <>
-    <FormButtons/>
+    <FormButtons
+    nome={nome}
+    senha={senha}
+    handleName={handleNome}
+    handleSenha={handleSenha}
+
+    />
     <Table/>
     </>
   )
