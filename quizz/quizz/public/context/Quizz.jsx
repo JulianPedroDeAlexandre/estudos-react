ce0e3161a5ef61a5c90f8e63081304cc0537a0e0
Julian Pedro De Alexandre <julian.zep@gmail.com>
2026-01-22 21:47:33 -0300

    feat: switch case de tela

--- quizz/quizz/public/context/Quizz.jsx
+++ quizz/quizz/public/context/Quizz.jsx
@@ -1,27 +1,36 @@
 import { createContext, useContext, useReducer } from "react";
 export const QuizzContext = createContext();
+import questions from "../data/questions"
 
-const initialState ={
+const initialState = {
     gameStage: "start",
     questions: [],
     currentQuestion: 0,
-    score:0
+    score: 0
 };
 
 const quizzReducer = (state, action) => {
-    switch(action.type){
+
+    switch (action.type) {
+        case "START_GAME":
+            return {
+                ...state,
+                gameStage: "playing",
+                questions,
+            };
         default:
             return state;
     }
+
 };
 
-export function QuizzProvider({children}){
-const [state, dispatch] = useReducer(quizzReducer, initialState);
+export function QuizzProvider({ children }) {
+    const [state, dispatch] = useReducer(quizzReducer, initialState);
 
-return(
-    <QuizzContext.Provider value={{ state, dispatch}}>
-        {children}
-    </QuizzContext.Provider>
-);
+    return (
+        <QuizzContext.Provider value={{ state, dispatch }}>
+            {children}
+        </QuizzContext.Provider>
+    );
 
 }
