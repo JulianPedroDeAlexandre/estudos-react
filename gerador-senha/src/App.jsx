5c89c9f52d1800966fe9e47fdb79dfcb6a6f0f17
Julian Pedro De Alexandre <julian.zep@gmail.com>
2026-01-23 22:02:26 -0300

    feat: gerar senha

--- gerador-senha/src/App.jsx
+++ gerador-senha/src/App.jsx
@@ -1,13 +1,27 @@
-
-
+import { useState } from "react"
 function App() {
 
+  const [password, setPassword] = useState("")
+
+  function generate() {
+    const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
+    const length = 12;
+    let newpassworld = "";
+    for (let i=0; i<length; i++) {
+      //gera numero float aleatorio maior que 0 e menor que 1, que tem varias casas, e multiplica pelo
+      //comprimento do characters, exemplo: 0,2356 * 90 daria 21.204 o math.floor aredonda pra 21
+      //pelo o caracter de posição 21 da variavel
+      const position = Math.floor(Math.random() * characters.length)
+      newpassworld += characters[position]
+    }
+    setPassword(newpassworld)
+  }
   return (
     <div className="app">
       <h1>Gerador de senha</h1>
-      <button>Gerar</button>
+      <button onClick={generate}>Gerar</button>
       <button>Copiar</button>
-      <div>senha</div>
+      <div>{password}</div>
     </div>
   )
 }
