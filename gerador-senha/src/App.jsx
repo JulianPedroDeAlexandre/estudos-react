4fd81dc9b71b5f2ef712993811dea7cff636c31d
Julian Pedro De Alexandre <julian.zep@gmail.com>
2026-01-23 21:27:47 -0300

    feat: testes iniciais

--- gerador-senha/src/App.jsx
+++ gerador-senha/src/App.jsx
@@ -3,9 +3,12 @@
 function App() {
 
   return (
-    <>
-     
-    </>
+    <div className="app">
+      <h1>Gerador de senha</h1>
+      <button>Gerar</button>
+      <button>Copiar</button>
+      <div>senha</div>
+    </div>
   )
 }
 
