e8cac508c7cc9c8be93cbf5963eb5f7ab1be2f45
Julian Pedro De Alexandre <julian.zep@gmail.com>
2026-01-26 19:42:01 -0300

    feat: cadastro e login

--- exefixação/vite-project/src/App.jsx
+++ exefixação/vite-project/src/App.jsx
@@ -1,10 +1,71 @@
 import Form from "./components/form/Form"
 import "../src/globals/globals.css"
+import Table from "./components/Table/Table"
+import { useState } from "react"
+
 function App() {
+
+  const [nome, setNome] = useState("");
+  const [senha, setSenha] = useState("");
+  const [nomeTabela, setNomeTabela] = useState("");
+  const [senhaTabela, setSenhaTabela] = useState("");
+
+  const handleName = (e) => {
+    setNome(e.target.value)
+  }
+
+  const handleSenha = (e) => {
+    setSenha(e.target.value)
+  }
+
+  onkeydown = (e) => {
+    if (e.key === "Enter" && !nomeTabela && !senhaTabela) {
+      cadastrar();
+    }
+    if (e.key === "Enter" && nomeTabela && senhaTabela) {
+      entrar();
+    }
+  }
+
+  function entrar() {
+    if (!nome || !senha) {
+      alert("Preencha o formulario")
+    } else {
+      if (nome === nomeTabela && senha === senhaTabela) {
+        alert("login efetuado com sucesso")
+      } else {
+        alert("erro ao efetuar login, nome ou senha incorretos")
+      }
+    }
+
+  }
+
+  function cadastrar() {
+    if (!nome || !senha) {
+      alert("Preencha o formulario")
+    } else {
+      setNomeTabela(nome)
+      console.log(nome)
+      setSenhaTabela(senha)
+      console.log(senha)
+
+    }
+  }
 
   return (
     <>
-     <Form></Form>
+      <Form
+        nome={nome}
+        senha={senha}
+        handleName={handleName}
+        handleSenha={handleSenha}
+        entrar={entrar}
+        cadastrar={cadastrar}
+      />
+      <Table
+        nomeTabela={nomeTabela}
+        senhaTabela={senhaTabela}
+      />
     </>
   )
 }
