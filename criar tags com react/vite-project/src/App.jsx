c0d30b07b8cc710264459e54793e3de8ca354114
Julian Pedro De Alexandre <julian.zep@gmail.com>
2026-01-28 10:11:41 -0300

    fix: limpeza inicial do projeto Vite (arquivos default e ajustes de estrutura)

---
+++ criar tags com react/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import "../src/globals/style.css"
+import Form from "./form/Form"
+import Table from "./table/Table"
+
+function App() {
+
+  return (
+    <>
+     <Form/>
+     <Table/>
+    </>
+  )
+}
+
+export default App
